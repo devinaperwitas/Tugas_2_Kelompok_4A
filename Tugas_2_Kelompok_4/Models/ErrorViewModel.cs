fc33981176f5696069ba25376cea1fda01f8c551
astratechlearn <devinaperwitas@gmail.com>
2023-10-02 08:07:00 +0700

    Add project files.

---
+++ Tugas_2_Kelompok_4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tugas_2_Kelompok_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
